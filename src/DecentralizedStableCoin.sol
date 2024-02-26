ae03a3a7ea2a032c80a3ef1d848ff1b645f7e0b1
Niranjan <niranjanthapa644122@gmail.com>
2024-02-26 13:59:57 +0545

    feat: mint and get the collateral value

--- src/DecentralizedStableCoin.sol
+++ src/DecentralizedStableCoin.sol
@@ -19,7 +19,7 @@ contract DecentralizedStableCoin is ERC20Burnable, Ownable {
     error DecentralizedStableCoin__BurnAmountExceedsBalance();
     error DecentralizedStableCoin__NotZeroAddress();
 
-    constructor() ERC20("DecentralizedStableCoin", "DSC") Ownable(msg.sender) {}
+    constructor() ERC20("DecentralizedStableCoin", "DSC") Ownable() {}
 
     function burn(uint256 _amount) public override onlyOwner {
         uint256 balance = balanceOf(msg.sender);
